a8a9a4ea9abd06aa720c8179a7fb3b7c26754f58
rk7892 <ranjithraina7952@gmail.com>
2024-01-23 13:36:36 +0530

    UI fixed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,38 +6,28 @@ import { HomeComponent } from './home/home.component';
 import { LogoutComponent } from './logout/logout.component';
 import { AddClientComponent } from './add-client/add-client.component';
 export const routes: Routes = [
-    {
-        path: "home",
-        component: HomeComponent,
-    },
-    {
-        path: "logout",
-        component: LogoutComponent,
-    },
-    {
-        path: "client-list",
-        component: ClientListComponent,
-        children:[
-            {
-                path: "client-details",
-                component: ClientDetailsComponent,
-            },
-            {
-                path:"add-client",
-                component: AddClientComponent
-            },
-        ]
-    },
-    // {
-    //     path: "client-details",
-    //     component: ClientDetailsComponent,
-    // },
-    // {
-    //     path:"add-client",
-    //     component: AddClientComponent
-    // },
-    {
-        path: "edit-client",
-        component: EditClientComponent,
-    },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'logout',
+    component: LogoutComponent,
+  },
+  {
+    path: 'client-list',
+    component: ClientListComponent,
+  },
+  {
+    path: 'client-list/client-details',
+    component: ClientDetailsComponent,
+  },
+  {
+    path: 'client-list/add-client',
+    component: AddClientComponent,
+  },
+  {
+    path: 'client-list/edit-client',
+    component: EditClientComponent,
+  },
 ];
